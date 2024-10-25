9d70d1b03cdf91d8589db2d6dbdfafc6066a8a18
duyan77 <anbui5948@gmail.com>
2024-10-25 14:29:01 +0700

    navbar ui

--- saleapp/index.py
+++ saleapp/index.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
-
+from dao import load_categories
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    categories = load_categories()
+    return render_template('index.html', categories=categories)
 
 
 if __name__ == '__main__':
