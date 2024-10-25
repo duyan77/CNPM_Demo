d75a00fdddef8995ee6541796e0fa94e28dc96d6
duyan77 <anbui5948@gmail.com>
2024-10-25 16:19:52 +0700

    ui

--- saleapp/dao.py
+++ saleapp/dao.py
@@ -4,3 +4,13 @@ import json
 def load_categories():
     with open('data/categories.json', encoding='utf-8') as f:
         return json.load(f)
+
+
+def load_products(q=None):
+    with open('data/products.json', encoding='utf-8') as p:
+        products = json.load(p)
+
+        if q:
+            products = [p for p in products if p['name'].find(q) >= 0]
+
+        return products
--- saleapp/index.py
+++ saleapp/index.py
@@ -1,13 +1,15 @@
-from flask import Flask, render_template
-from dao import load_categories
+from flask import Flask, render_template, request
+import dao
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    categories = load_categories()
-    return render_template('index.html', categories=categories)
+    q = request.args.get("q")
+    products = dao.load_products(q)
+    categories = dao.load_categories(products, q)
+    return render_template('index.html', categories=categories, products=products)
 
 
 if __name__ == '__main__':
